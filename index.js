635619a5f2fee5ece2412639ddc5aad1680613a0
Daniela Ruiz <daru015@gmail.com>
2018-07-16 14:58:17 +0200

    adding sattic react project

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
 const express = require('express');
+let path = require('path');
 const app = express();
-const fs = require('fs');
-const https = require('https');
 const http = require('http').Server(app);
-const io = require('socket.io')(https);
+const io = require('socket.io')(http);
 
 let config = require('./config.json');
-let httpsOptions = {
-    key: fs.readFileSync('./localhost.key', 'utf8'),
-    cert: fs.readFileSync('./localhost.crt', 'utf8'),
-};
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    if ('OPTIONS' == req.method) {
-        res.sendStatus(200);
-    }
-    else {
-        next();
-    }
-});
-
-app.get('/', function(req, res) {
-    res.send('Hello World!');
-});
+app.use('/',express.static(path.join(__dirname, 'static')));
 
 function onConnection(socket) {
     socket.on('drawing', (data) => {
@@ -36,4 +17,4 @@ function onConnection(socket) {
 
 io.on('connection', onConnection);
 
-https.createServer(httpsOptions, app).listen(config.port, () => console.log('listening on port ' + config.port));
+http.listen(config.port, () => console.log('listening on port ' + config.port));

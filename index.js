a4e51be2857a5327af66aa5edfb6f9b0bf0809a7
Daniela Ruiz <daru015@gmail.com>
2018-07-16 09:00:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-let config = './config.json';
+let config = require('./config.json');
 
 app.get('/', function (req, res) {
     res.send('Hello World!');

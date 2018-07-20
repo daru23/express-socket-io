702cef241adcad309d1033bd2fb3fac20a0faa2d
Daniela Ruiz <daru015@gmail.com>
2018-07-20 16:44:06 +0200

    adding user subscription

--- index.js
+++ index.js
@@ -8,11 +8,42 @@ let config = require('./config.json');
 
 app.use('/',express.static(path.join(__dirname, 'static')));
 
+let numUsers = 0;
+
 function onConnection(socket) {
+    let addedUser = false;
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', (username) => {
+        if (addedUser) return;
+
+        // we store the username in the socket session for this client
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+    // when the user disconnects.. perform this
+    socket.on('disconnect', () => {
+        if (addedUser) {
+            --numUsers;
+            // echo globally that this client has left
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
     socket.on('drawing', (data) => {
-        console.log(data);
         return socket.broadcast.emit('drawing', data);
     });
+
 }
 
 io.on('connection', onConnection);
